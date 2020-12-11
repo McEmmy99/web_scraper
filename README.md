82fd9354dffc902a5ceb078843346e49338574da
Emmanuel ALabi <alabiemmanuel016@gmail.com>
2020-12-11 11:02:16 +0100

    Scraped the title of books on the homepage

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 THIS IS A BOT TO CRAWL A RECIPE WEBSITE.
 
 I WILL CREATE AN API FROM THE DATA.JSON FILE
+
+THIS IS THE EXERCISE FOR THE TUTORIAL AT https://www.digitalocean.com/community/tutorials/how-to-scrape-a-website-using-node-js-and-puppeteer#top
--- pageScraper.js
+++ pageScraper.js
@@ -8,6 +8,18 @@ const scraperObject = {
         console.log(`Navigating to ${this.url}...`);
         //.goto() navigates to the url homepage
         await page.goto(this.url);
+        //Wait for the required DOM to be rendered
+        await page.waitForSelector('.page_inner');
+        //Get the link to all the required books
+        //Be sure to only reurn a string or number with .$$eval() or .$eval() methods
+        let urls = await page.$$eval('section ol > li', links => {
+            //Make sure the book to be scrapped is in stock
+            links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
+            //Extract the links from the data
+            links = links.map(el => el.querySelector('h3 > a').href)
+            return links;
+        });
+        console.log(urls);
     }
 }
 
